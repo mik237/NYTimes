1f8a43b4a17ea99b55919dee5325a98a8ea46e17
muhammad ibrahim <muhammad.ibrahim@innovativesolutions.ae>
2021-05-01 15:06:59 +0400

    removed unwanted imports

--- app/src/main/java/com/ibrahim/ny_times_demo/ui/fragments/PopularArticlesListFragment.kt
+++ app/src/main/java/com/ibrahim/ny_times_demo/ui/fragments/PopularArticlesListFragment.kt
@@ -1,7 +1,6 @@
 package com.ibrahim.ny_times_demo.ui.fragments
 
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
